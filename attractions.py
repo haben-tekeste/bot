b0d61b995939d8423458f34fe1776c4c43a8267e
mekson bravo <95582758+haben-tekeste@users.noreply.github.com>
2023-09-19 08:21:01 +0000

    updates

---
+++ attractions.py
@@ -0,0 +1,84 @@
+uae_attractions = {
+    "Abu Dhabi": {
+        "1. Sheikh Zayed Grand Mosque": "One of the largest mosques in the world known for its stunning architecture.",
+        "2. Louvre Abu Dhabi": "An art and civilization museum with a remarkable collection of artworks.",
+        "3. Ferrari World": "An indoor amusement park with thrilling rides and Ferrari-themed attractions.",
+    },
+    "Dubai": {
+        "1. Burj Khalifa": "The world's tallest skyscraper offering panoramic views from the observation deck.",
+        "2. Palm Jumeirah": "A man-made palm-shaped island with luxury resorts and attractions.",
+        "3. Dubai Mall": "A massive shopping and entertainment complex with an indoor ice rink and aquarium.",
+    },
+    "Sharjah": {
+        "1. Sharjah Museum of Islamic Civilization": "A museum displaying Islamic artifacts and art.",
+        "2. Sharjah Arts Area": "A cultural district with galleries, theaters, and cafes.",
+        "3. Sharjah Desert Park": "A wildlife and natural history park showcasing the desert ecosystem.",
+    },
+    "Ras Al Khaimah": {
+        "1. Jebel Jais": "UAE's highest mountain with scenic views and adventure activities.",
+        "2. Al Qasimi Palace": "A historic palace offering a glimpse into the emirate's heritage.",
+        "3. Saqr Park": "A family-friendly park with playgrounds and green spaces.",
+    },
+    "Ajman": {
+        "1. Ajman Museum": "A museum housed in an 18th-century fort showcasing Ajman's history.",
+        "2. Ajman Beach": "A peaceful beach for relaxation and water sports.",
+        "3. Al Zorah Nature Reserve": "A protected mangrove forest with walking trails and wildlife.",
+    },
+    "Umm Al Quwain": {
+        "1. Dreamland Aqua Park": "A water park with slides, pools, and camping facilities.",
+        "2. Umm Al Quwain Museum": "A museum with exhibits on the emirate's past.",
+        "3. UAQ Marine Club": "A venue for water sports and fishing.",
+    },
+    "Fujairah": {
+        "1. Al Badiyah Mosque": "The UAE's oldest mosque, known for its unique architecture.",
+        "2. Fujairah Fort": "A historic fort offering insights into the emirate's history.",
+        "3. Snoopy Island": "A popular snorkeling and diving spot with diverse marine life.",
+    }
+}
+
+uae_attractions_by_interest = {
+    "Nature": {
+        "1. Jebel Jais (Ras Al Khaimah)": "UAE's highest mountain with scenic views and adventure activities.",
+        "2. Al Zorah Nature Reserve (Ajman)": "A protected mangrove forest with walking trails and wildlife.",
+        "3. Snoopy Island (Fujairah)": "A popular snorkeling and diving spot with diverse marine life."
+    },
+    "Outdoor Adventure": {
+        "1. Desert Safari (Various Emirates)": "Exciting dune bashing and desert adventures in the UAE deserts.",
+        "2. Hatta Pools (Dubai)": "Natural rock pools in the Hajar Mountains, ideal for hiking and swimming.",
+        "3. Wadi Adventure (Al Ain)": "An adventure park with rafting, kayaking, and zip-lining."
+    },
+    "Indoor Attractions": {
+        "1. Burj Khalifa (Dubai)": "The world's tallest skyscraper with an observation deck.",
+        "2. Louvre Abu Dhabi (Abu Dhabi)": "An art and civilization museum with a remarkable collection of artworks.",
+        "3. Dubai Mall (Dubai)": "A massive shopping and entertainment complex with an indoor ice rink and aquarium."
+    },
+    "Beaches": {
+        "1. Jumeirah Beach (Dubai)": "A pristine sandy beach with views of the iconic Burj Al Arab.",
+        "2. Corniche Beach (Abu Dhabi)": "A popular beachfront promenade with parks and cafes.",
+        "3. Al Mamzar Beach Park (Dubai)": "A park with beautiful beaches, lagoons, and picnic areas."
+    },
+    "Cultural": {
+        "1. Sheikh Zayed Grand Mosque (Abu Dhabi)": "One of the largest mosques in the world known for its stunning architecture.",
+        "2. Al Fahidi Historic District (Dubai)": "A historic neighborhood with traditional wind-tower architecture.",
+        "3. Al Badiyah Mosque (Fujairah)": "The UAE's oldest mosque, known for its unique architecture."
+    }
+}
+
+
+uae_attractions_by_budget = {
+    "Budget-Friendly": {
+        "1. Al Fahidi Historic District (Dubai)": "Explore the charming streets of Old Dubai with affordable or free attractions.",
+        "2. Ajman Beach (Ajman)": "Enjoy a day at the beach in Ajman without breaking the bank.",
+        "3. Al Badiyah Mosque (Fujairah)": "Visit the UAE's oldest mosque, which is often free to enter."
+    },
+    "Mid-Range": {
+        "1. Desert Safari (Various Emirates)": "Experience an exciting desert safari with various pricing options.",
+        "2. Dubai Aquarium & Underwater Zoo (Dubai)": "Explore marine life at a reasonable cost in Dubai Mall.",
+        "3. Ferrari World (Abu Dhabi)": "Enjoy a day of thrilling rides and attractions at this mid-range theme park."
+    },
+    "Luxury": {
+        "1. Burj Khalifa (Dubai)": "Treat yourself to the luxury of visiting the world's tallest skyscraper.",
+        "2. Emirates Palace (Abu Dhabi)": "Experience opulence and luxury at this iconic hotel.",
+        "3. Zaya Nurai Island (Abu Dhabi)": "Stay at a private island resort for the ultimate luxury experience."
+    }
+}
--- bot.py
+++ bot.py
@@ -1,3 +1,9 @@
+from slack_bolt import App
+from flask import Flask, request
+from slack_bolt.adapter.flask import SlackRequestHandler
+import requests
+import re
+import attractions
 
 SLACK_TOKEN = "xoxb-5817394942737-5790226507639-yR8GjqScVYXCVSq71AZHSiER"
 SIGNING_SECRET = "b2d6813296153e31455aff7c555e3230"
@@ -6,11 +12,6 @@ url = "https://api.wit.ai/message"
 
 conversation_states = {}
 
-from slack_bolt import App
-from flask import Flask, request
-from slack_bolt.adapter.flask import SlackRequestHandler
-import requests
-import re
 
 app = App(
     token=SLACK_TOKEN,
@@ -25,60 +26,94 @@ def greet_and_get_names(body, say, logger):
     event = body.get('event')
     user_id = event.get('user')
     channel = event.get('channel')
-    print("here")
 
-    if channel not in conversation_states:
-        conversation_states[channel] = {"users": [], "step": 1}
+    # if channel not in conversation_states:
+    #     conversation_states[channel] = {"users": [], "step": 1}
 
-    if user_id not in conversation_states[channel]["users"]:
-        conversation_states[channel]["users"].append(user_id)
+    # if user_id not in conversation_states[channel]["users"]:
+    #     conversation_states[channel]["users"].append(user_id)
 
-        if len(conversation_states[channel]["users"]) == 1:
-            response = f"Hello, <@{user_id}>! Please introduce yourself."
-        else:
-            response = f"Hello, <@{user_id}>! Please introduce yourself as well."
+    #     if len(conversation_states[channel]["users"]) == 1:
+    #         response = f"Hello, <@{user_id}>! Please introduce yourself."
+    #     else:
+    #         response = f"Hello, <@{user_id}>! Please introduce yourself as well."
 
-        app.client.chat_postMessage(channel=channel, text=response)
+    #     app.client.chat_postMessage(channel=channel, text=response)
+
+    if channel not in conversation_states[channel]:
+        conversation_states[channel] = {}
+    
+    if user_id not in conversation_states[channel]:
+         conversation_states[channel][user_id] = {"step": 1, "introduced": False}
+         response = f"Hello, <@{user_id}>! My name is bot, and I'm happy to assist you today. Please share your vacations Preferences"
+         say(response)
 
 
-# Step 2: Initial Inquiry
-@app.message(re.compile(".*name.*"))
-def initial_inquiry(payload, say):
-    channel = payload["channel"]
-    user_id = payload["user"]
-    user_state = conversation_states.get(channel)
+# # Step 2: Initial Inquiry
+# @app.message(re.compile(".*name.*"))
+# def initial_inquiry(payload, say):
+#     channel = payload["channel"]
+#     user_id = payload["user"]
+#     user_state = conversation_states.get(channel)
 
-    if user_state and user_id in user_state["users"] and user_state["step"] == 1:
-        user_state["step"] = 2
-        say("What do you need or expect from your trip to the UAE?")
+#     if user_state and user_id in user_state["users"] and user_state["step"] == 1:
+#         user_state["step"] = 1.5
+#         say(f"What do you need or expect from your trip to the UAE? <@{user_id}>")
 
 
 # Step 3: Interest and Preferences
-@app.message(re.compile(".*interest.*"))
+@app.message(re.compile(r".*interest.*"))
 def get_interests(payload, say):
     channel = payload["channel"]
     user_id = payload["user"]
     user_state = conversation_states.get(channel)
 
-    if user_state and user_state["step"] == 3:
-        if len(conversation_states[channel]) >= 2:
-            # Both users have shared their interests
-            user_state["step"] = 4
-            say("Great! Now, let's talk about attractions based on your interests.")
+    # check if user has mentioned bot
+    if (user_state and user_state[user_id]):
+        if (user_state[user_id]['step'] == 1):
+            # user mentiones interests for the first time
+            say(f"Great! Now, let's talk about attractions based on your interests. <@{user_id}>")
+            user_state[user_id]['step'] = 2
+            # call a function that responds to interests 
+            # remind other user to share their interests
+            for (user in user_state):
+                if (user_state[user]['step'] == 1 and user != user_id):
+                    say(f"What about you <@{user_id}>, Please share your interests")
         else:
-            say("Thanks for sharing your interests. Please wait for the other user to share theirs.")
+            say(f"I have taken your interests earlier")
+    # if user_state and user_state["step"] == 3:
+    #     if len(conversation_states[channel]) >= 2:
+    #         # Both users have shared their interests
+    #         user_state["step"] = 4
+    #         say("Great! Now, let's talk about attractions based on your interests.")
+    #     else:
+    #         say("Thanks for sharing your interests. Please wait for the other user to share theirs.")
 
-# @app.message(re.compile("@bot"))
-# def bot_message(body, say, logger):
-    # logger.info(body)
-    # event = body.get('event')
-    # user_id = event.get('user')
-    # text = event.get('text')
-    # response = (requests.get(url, headers={"Authorization": "Bearer " + "WHQMO3BTFYAYJCDSLLP77RPTP6VJGQBD"},params={"q": text})).json()
-    # if (len(response['intents']) > 0 and response['intents'][0]['confidence'] > 0.7):
-    #     say("Intent --> "+response['intents'][0]['name']+ "")
-    # else:
-    #     say("I'm not sure how to respond to that.")
+
+@app.message(re.compile(r".*(suggest|recommend|propose|put forward).*"))
+def suggest_attractions(payload, say):
+    channel = payload["channel"]
+    user_id = payload["user"]
+    user_state = conversation_states.get(channel)
+
+    if (user_state and user_state[id]):
+        say(f"Sure, I would love to share some attractions. which emirate is do you want to visit please?")
+
+
+@app.message(re.compile(r".*(dubai|abu dhabi| ajman|fujairah|sharjah|ras al khaimah| umm al quwain).*"))
+def suggest_attraction_based_emirate(payload, say):
+    channel = payload["channel"]
+    user_id = payload["user"]
+    user_state = conversation_states.get(channel)
+    emirate = payload['text'].capitalize()
+
+    attractions_list = attractions.uae_attractions[emirate]
+    response = ""
+    if (user_state and user_state[id]):
+        say(f"Here are attractions in {emirate}")
+        for i, (attraction, description) in enumerate(attractions_list.items(), start=1):
+            response += f" {attraction}: {description} \n"
+        say(response)
 
 
 @app.event("message")
